2b0cac122e06078e94f25dbc11933c444d3c93ad
rblom6 <robbieblom00@gmail.com>
2022-07-09 13:46:06 -0400

    added some print statements. Changed main so that it can be run interactively.

--- main.py
+++ main.py
@@ -14,28 +14,33 @@ def main(args):
 
     Return: none
     """
-    (sector, industry) = (args[1],args[2])
+    sector = input("Enter a sector: ")
+    industry = input("Enter an industry: ")
 
+    print("Reading ticker metadata...")
     tTree = TickerTree("SP_500.xml")
     tTree.read_tree()
     industryTickers = tTree.get_industry_tickers(sector, industry)
 
+    print("Reading historical data...")
     tData = TickerData("SP500_ind.csv")
     tData.read_data()
     
+    print("Printing ticker, weighted avg price, and avg open...")
     for ticker in industryTickers:
         if ticker in tData.data:
             try:
                 wAvgPrice = tData.vwap(ticker)
                 avgOpen = tData.calc_avg_open(ticker)
             except:
-                print("One of your arguments caused an error.")
+                print("One of your inputs caused an error.")
                 return
-            if len(args) == 3:
-                print([ticker, wAvgPrice, avgOpen])
-            else:
-                csvout = csv.writer(open(args[3], "a"))
-                csvout.writerow([ticker, wAvgPrice, avgOpen])
+            print([ticker, wAvgPrice, avgOpen])
+            # if len(args) == 3:
+            #     print([ticker, wAvgPrice, avgOpen])
+            # else:
+            #     csvout = csv.writer(open(args[3], "a"))
+            #     csvout.writerow([ticker, wAvgPrice, avgOpen])
 
 
 if __name__ == "__main__":
